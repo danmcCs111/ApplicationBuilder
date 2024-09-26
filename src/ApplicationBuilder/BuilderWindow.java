41925eca29b7c2d529d66b9c727727c0181892a5
Daniel Boydelatour <danielboydelatour@yahoo.com>
2024-09-26 11:02:41 -0400

    add dropdown menu with widget selection

--- src/ApplicationBuilder/BuilderWindow.java
+++ src/ApplicationBuilder/BuilderWindow.java
@@ -1,11 +1,13 @@
 package ApplicationBuilder;
 
+import java.awt.BorderLayout;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,7 +26,7 @@ public class BuilderWindow extends JFrame {
 		
 //		addMenuButtons();
 		
-		setTitle(WidgetTextProperties.APPLICATION_TITLE.getPropertiesValue());
+		setTitle("ApplicationBuilder");
 		setLocation(150, 150);
 		
 //		BorderLayout bl = new BorderLayout();
@@ -41,68 +43,33 @@ public class BuilderWindow extends JFrame {
 //		this.setSize(getWindowWidth(), winHeight);
 //		this.addWindowListener(new RokuLauncherWindowListener(this));
 //		this.addComponentListener(new RokuLauncherComponentListener(this));
-		
-		this.setSize(480, 640);
-		
-		HashMap<String, ArrayList<String>> settersAndClass = generateMethodApiList("set");
-		for(String methodStr : settersAndClass.keySet())
-		{
-			LoggingMessages.printOut(settersAndClass.get(methodStr).toString() + ": " + methodStr);
-		}
-		
-		
+
 		HashMap<String, ArrayList<String>> classesAndSetters = generateClassesMethodApiList("set");
+		ArrayList<String> tmp = new ArrayList<String>(classesAndSetters.keySet());
+		String [] selections = tmp.toArray(new String [tmp.size()]);
 		for(String classStr : classesAndSetters.keySet())
 		{
 			for(String s : classesAndSetters.get(classStr))
 				LoggingMessages.printOut(classStr + ": " + s.toString());
 		}
+		JComboBox<String> classSelection = new JComboBox<String>(selections);
+		classSelection.setSize(100, 50);
+		classSelection.setVisible(true);
+
+		
+		this.add(classSelection, BorderLayout.NORTH);
+		
+		this.setSize(480, 640);
+		
+//		HashMap<String, ArrayList<String>> settersAndClass = generateUniqueMethodApiList("set");
+//		for(String methodStr : settersAndClass.keySet())
+//		{
+//			LoggingMessages.printOut(settersAndClass.get(methodStr).toString() + ": " + methodStr);
+//		}
+//		
+		
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	public HashMap<String, ArrayList<String>> generateMethodApiList(String methodPrefixFilter)
-	{
-		HashMap<String, ArrayList<String>> settersAndClass = new HashMap<String, ArrayList<String>>();
-		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
-		classes.add(JFrame.class);
-		classes.add(JPanel.class);
-		classes.add(JButton.class);
-		classes.add(JTextField.class);
-		classes.add(JLabel.class);
-		
-		for(Class<?> c : classes)
-		{
-			for (Method m : c.getMethods())
-			{
-				String methodName = m.getName();
-				String paramName = " [";
-				for (int i =0; i < m.getParameterCount(); i++)
-				{
-					Parameter p = m.getParameters()[i];
-					paramName += p.toString();
-					if(m.getParameterCount() > i+1)
-					{
-						paramName += ", ";
-					}
-				}
-				methodName += paramName + "]";
-				if(methodName.startsWith(methodPrefixFilter))
-				{
-					if(settersAndClass.containsKey(methodName))
-					{
-						settersAndClass.get(methodName).add(c.getName());
-					}
-					else
-					{
-						ArrayList<String> tmp = new ArrayList<String>();
-						tmp.add(c.getName());
-						settersAndClass.put(methodName, tmp);
-					}
-				}
-			}
-		}
-		return settersAndClass;
 	}
 	
 	public HashMap<String, ArrayList<String>> generateClassesMethodApiList(String methodPrefixFilter)
@@ -146,5 +113,49 @@ public class BuilderWindow extends JFrame {
 			}
 		}
 		return classMethods;
+	}
+	
+	public HashMap<String, ArrayList<String>> generateUniqueMethodApiList(String methodPrefixFilter)
+	{
+		HashMap<String, ArrayList<String>> settersAndClass = new HashMap<String, ArrayList<String>>();
+		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
+		classes.add(JFrame.class);
+		classes.add(JPanel.class);
+		classes.add(JButton.class);
+		classes.add(JTextField.class);
+		classes.add(JLabel.class);
+		
+		for(Class<?> c : classes)
+		{
+			for (Method m : c.getMethods())
+			{
+				String methodName = m.getName();
+				String paramName = " [";
+				for (int i =0; i < m.getParameterCount(); i++)
+				{
+					Parameter p = m.getParameters()[i];
+					paramName += p.toString();
+					if(m.getParameterCount() > i+1)
+					{
+						paramName += ", ";
+					}
+				}
+				methodName += paramName + "]";
+				if(methodName.startsWith(methodPrefixFilter))
+				{
+					if(settersAndClass.containsKey(methodName))
+					{
+						settersAndClass.get(methodName).add(c.getName());
+					}
+					else
+					{
+						ArrayList<String> tmp = new ArrayList<String>();
+						tmp.add(c.getName());
+						settersAndClass.put(methodName, tmp);
+					}
+				}
+			}
+		}
+		return settersAndClass;
 	}
 }
