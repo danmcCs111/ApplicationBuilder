e86cd66ff878719092cb015acc8e6f46a8637167
Daniel Boydelatour <danielboydelatour@yahoo.com>
2024-09-26 12:18:19 -0400

    update to display methods per widget component

--- src/ApplicationBuilder/BuilderWindow.java
+++ src/ApplicationBuilder/BuilderWindow.java
@@ -1,49 +1,40 @@
 package ApplicationBuilder;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-
-import Properties.WidgetTextProperties;
 
 public class BuilderWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private static final String SOURCE_FILE = "src\\ApplicationBuilder\\data\\WidgetBuild.xml";
 	
+	private HashMap<String, JList<?>> listOfComponentMethods = new HashMap<String, JList<?>>();
+	private JScrollPane scrPane = null;
+	private JPanel innerPanel2 = new JPanel();
+	private JList<?> componentMethods = null;
+	
 	public BuilderWindow()
 	{
 		new WidgetBuildController(SOURCE_FILE);
 		
-//		addMenuButtons();
-		
 		setTitle("ApplicationBuilder");
 		setLocation(150, 150);
 		
-//		BorderLayout bl = new BorderLayout();
-//		scrPane = new JScrollPane(innerPanel);
-//		BorderLayout bl2 = new BorderLayout();
-//		innerPanel2.setLayout(bl2);
-//		innerPanel2.add(scrPane, BorderLayout.NORTH);
-//		this.setLayout(bl);
-//		this.add(innerPanel2, BorderLayout.CENTER);
-//		
-//		createNavigationButtons();
-//		addChannelButtons();
-//		
-//		this.setSize(getWindowWidth(), winHeight);
-//		this.addWindowListener(new RokuLauncherWindowListener(this));
-//		this.addComponentListener(new RokuLauncherComponentListener(this));
-
 		HashMap<String, ArrayList<String>> classesAndSetters = generateClassesMethodApiList("set");
 		ArrayList<String> tmp = new ArrayList<String>(classesAndSetters.keySet());
 		String [] selections = tmp.toArray(new String [tmp.size()]);
@@ -53,23 +44,49 @@ public class BuilderWindow extends JFrame {
 				LoggingMessages.printOut(classStr + ": " + s.toString());
 		}
 		JComboBox<String> classSelection = new JComboBox<String>(selections);
-		classSelection.setSize(100, 50);
+		classSelection.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clearInnerPanels();
+				componentMethods = listOfComponentMethods.get(classSelection.getSelectedItem());
+				scrPane = new JScrollPane(componentMethods);
+				innerPanel2.add(scrPane, BorderLayout.CENTER);
+				BuilderWindow.this.add(innerPanel2, BorderLayout.CENTER);
+				
+				LoggingMessages.printOut("Method output for class: " + classSelection.getSelectedItem().toString());
+				BuilderWindow.this.paintComponents(BuilderWindow.this.getGraphics());
+			}
+		});
 		classSelection.setVisible(true);
+		
+		
+		for (String c : classesAndSetters.keySet())
+		{
+			ArrayList<String> methods =	classesAndSetters.get(c);
+			Collections.sort(methods);//Sort list...
+			listOfComponentMethods.put(c, new JList(methods.toArray()));
+		}
+		componentMethods = listOfComponentMethods.get(classSelection.getSelectedItem());
+		LoggingMessages.printOut("Method output for class: " + classSelection.getSelectedItem().toString());
+		BorderLayout bl = new BorderLayout();
+		scrPane = new JScrollPane(componentMethods);
+		BorderLayout bl2 = new BorderLayout();
+		innerPanel2.setLayout(bl2);
+		innerPanel2.add(scrPane, BorderLayout.CENTER);
+		this.setLayout(bl);
+		this.add(innerPanel2, BorderLayout.CENTER);
 
 		
 		this.add(classSelection, BorderLayout.NORTH);
-		
 		this.setSize(480, 640);
-		
-//		HashMap<String, ArrayList<String>> settersAndClass = generateUniqueMethodApiList("set");
-//		for(String methodStr : settersAndClass.keySet())
-//		{
-//			LoggingMessages.printOut(settersAndClass.get(methodStr).toString() + ": " + methodStr);
-//		}
-//		
-		
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private void clearInnerPanels()
+	{
+		innerPanel2.removeAll();
+		scrPane.removeAll();
+		this.remove(innerPanel2);
 	}
 	
 	public HashMap<String, ArrayList<String>> generateClassesMethodApiList(String methodPrefixFilter)
@@ -113,49 +130,5 @@ public class BuilderWindow extends JFrame {
 			}
 		}
 		return classMethods;
-	}
-	
-	public HashMap<String, ArrayList<String>> generateUniqueMethodApiList(String methodPrefixFilter)
-	{
-		HashMap<String, ArrayList<String>> settersAndClass = new HashMap<String, ArrayList<String>>();
-		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
-		classes.add(JFrame.class);
-		classes.add(JPanel.class);
-		classes.add(JButton.class);
-		classes.add(JTextField.class);
-		classes.add(JLabel.class);
-		
-		for(Class<?> c : classes)
-		{
-			for (Method m : c.getMethods())
-			{
-				String methodName = m.getName();
-				String paramName = " [";
-				for (int i =0; i < m.getParameterCount(); i++)
-				{
-					Parameter p = m.getParameters()[i];
-					paramName += p.toString();
-					if(m.getParameterCount() > i+1)
-					{
-						paramName += ", ";
-					}
-				}
-				methodName += paramName + "]";
-				if(methodName.startsWith(methodPrefixFilter))
-				{
-					if(settersAndClass.containsKey(methodName))
-					{
-						settersAndClass.get(methodName).add(c.getName());
-					}
-					else
-					{
-						ArrayList<String> tmp = new ArrayList<String>();
-						tmp.add(c.getName());
-						settersAndClass.put(methodName, tmp);
-					}
-				}
-			}
-		}
-		return settersAndClass;
 	}
 }
