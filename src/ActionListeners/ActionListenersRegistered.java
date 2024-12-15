35371557de9b93f367a9cd8e7d604f835afbaaeb
danmcCs111 <danmcCs111@gmail.com>
2024-12-15 12:00:09 -0500

    add new ActionListener stubs

--- src/ActionListeners/ActionListenersRegistered.java
+++ src/ActionListeners/ActionListenersRegistered.java
@@ -4,9 +4,14 @@ import java.awt.event.ActionListener;
 
 public enum ActionListenersRegistered {
 
-		ComponentComboBoxActionListener(ComponentComboBoxActionListener.class, ""),
-		OpenDetailsActionListener(OpenDetailsActionListener.class, ""),
-		OpenParameterEditorActionListener(OpenParameterEditorActionListener.class, "");
+	ComponentComboBoxActionListener(ComponentComboBoxActionListener.class, "ComponentComboBoxActionListener"),
+	OpenDetailsActionListener(OpenDetailsActionListener.class, "OpenDetailsActionListener"),
+	OpenParameterEditorActionListener(OpenParameterEditorActionListener.class, "OpenParameterEditorActionListener"),
+	MinimizeActionListener(MinimizeActionListener.class, "MinimizeActionListener"),
+	NavigationButtonActionListener(NavigationButtonActionListener.class, "NavigationButtonActionListener"),
+	ReloadActionListener(ReloadActionListener.class, "ReloadActionListener"),
+	ExitActionListener(ExitActionListener.class, "ExitActionListener"),
+	LaunchActionListener(LaunchActionListener.class, "LaunchActionListener");
 	
 	private Class<? extends ActionListener> clazz;
 	private String classXmlText;
