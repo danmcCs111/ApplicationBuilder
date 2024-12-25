97904302bd4fe4c1a3f0f3bb436cbdb5f6ed8f18
danmcCs111 <danmcCs111@gmail.com>
2024-12-25 08:46:19 -0500

    move method arg def to constant in parent class for extended attributes

--- src/WidgetExtensions/ExtendedAttributeStringParam.java
+++ src/WidgetExtensions/ExtendedAttributeStringParam.java
@@ -2,13 +2,15 @@ package WidgetExtensions;
 
 public class ExtendedAttributeStringParam 
 {
+	public static final String METHOD_ARG_DEF = " [java.lang.String arg0]";
+	
 	public static  String getMethodDefinition(String clazzName) 
 	{
 		String [] className = clazzName.split("\\.");
 		String methodDef = className[className.length-1];
 		String methodBeginCase = methodDef.substring(0, 1);
 		
-		methodDef = methodBeginCase.toLowerCase() + methodDef.substring(1, methodDef.length()) + " [java.lang.String arg0]";
+		methodDef = methodBeginCase.toLowerCase() + methodDef.substring(1, methodDef.length()) + METHOD_ARG_DEF;
 		
 		return methodDef;
 	}
