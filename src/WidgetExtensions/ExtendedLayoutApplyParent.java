b982e522fb8d2714f7c629d2b7cc4de04b6f2e7f
danmcCs111 <danmcCs111@gmail.com>
2025-01-05 07:01:09 -0500

    update layout value in xml and add layout apply to extended class

--- src/WidgetExtensions/ExtendedLayoutApplyParent.java
+++ src/WidgetExtensions/ExtendedLayoutApplyParent.java
@@ -1,5 +1,11 @@
 package WidgetExtensions;
 
+import java.awt.Component;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.swing.JFrame;
+
 import ApplicationBuilder.WidgetBuildController;
 import WidgetUtility.WidgetCreatorProperty;
 
@@ -7,8 +13,37 @@ public class ExtendedLayoutApplyParent implements ExtendedAttributeStringParam
 {
 
 	@Override
-	public void applyMethod(String arg0, WidgetBuildController wbc, WidgetCreatorProperty widgetProperties) {
-		// TODO Auto-generated method stub
+	public void applyMethod(String arg0, WidgetBuildController wbc, WidgetCreatorProperty widgetProperties) 
+	{
+		WidgetCreatorProperty parentProp = wbc.findRef(widgetProperties.getParentRef());
+		Object parentComp = parentProp.getInstance();
+		Object comp = widgetProperties.getInstance();
 		
+		invoke(parentComp, comp, arg0);
+	}
+	
+	//Component, constraints
+	private static void invoke(Object parentComp, Object comp, String arg0)
+	{
+		Method m;
+		Class<?> [] cs = new Class<?> [] {Component.class, Object.class}; 
+		Object [] os = new Object [] {comp, arg0};
+		
+		try {
+			m = parentComp.getClass().getMethod("add", cs);
+			m.invoke(parentComp, os);
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
