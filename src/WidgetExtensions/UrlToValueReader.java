da9e5fab472f9e1ae6d6c576a65b5aad49d075bc
danmcCs111 <danmcCs111@gmail.com>
2025-01-05 19:14:06 -0500

    overloaded method broken, fix. move delimiter to constant, fix file seperator reference

--- src/WidgetExtensions/UrlToValueReader.java
+++ src/WidgetExtensions/UrlToValueReader.java
@@ -2,19 +2,23 @@ package WidgetExtensions;
 
 import java.io.File;
 
+import ApplicationBuilder.LoggingMessages;
 import Properties.PropertiesFileLoader;
 
 public class UrlToValueReader 
 {
+	private static final String URL_DELIMITER = "=";
+	
 	public static String parse(String filename, String path)
 	{
-		return parse(path + File.pathSeparator + filename, "=");
+		return parse(path + File.separator + filename);
 	}
 	
 	public static String parse(String filenameAndPath)
 	{
 		String value = null;
-		value = PropertiesFileLoader.readProperties(filenameAndPath, "=").get("URL");
+		LoggingMessages.printOut(filenameAndPath);
+		value = PropertiesFileLoader.readProperties(filenameAndPath, URL_DELIMITER).get("URL");
 		return value;
 	}
 	
