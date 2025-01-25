a7bb36829a2a7b6a536889e992403604a056c21a
danmcCs111 <danmcCs111@gmail.com>
2025-01-25 01:34:11 -0500

    remove Action listeners registered file

--- src/ClassDefintions/ActionListenerConverter.java
+++ src/ClassDefintions/ActionListenerConverter.java
@@ -2,26 +2,27 @@ package ClassDefintions;
 
 import java.awt.event.ActionListener;
 
-import ActionListeners.ActionListenersRegistered;
 import ApplicationBuilder.LoggingMessages;
 
 public class ActionListenerConverter implements StringToObjectConverter 
 {
 	public static ActionListener getActionListener(String arg0)
 	{
-		ActionListenersRegistered alr = ActionListenersRegistered.getActionListener(arg0);
-		Class<? extends ActionListener> actionListener = alr.getClazz();
 		ActionListener al = null;
 		try {
-			al = actionListener.newInstance();
+			Class<?> c = Class.forName(arg0);
+			al = (ActionListener) c.newInstance();
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		LoggingMessages.printOut("ActionListener name: " + alr.name());
+		LoggingMessages.printOut("ActionListener name: " + al.getClass().toString());
 		return al;
 	}
 
--- src/WindowListeners/ProcessExecutorWindowListener.java
+++ src/WindowListeners/ProcessExecutorWindowListener.java
@@ -12,4 +12,10 @@ public class ProcessExecutorWindowListener extends WindowAdapter
 	{
 		LaunchActionListener.destroyRunningProcess();
 	}
+	
+	@Override 
+	public void windowClosed(WindowEvent e)
+	{
+		LaunchActionListener.destroyRunningProcess();
+	}
 }
