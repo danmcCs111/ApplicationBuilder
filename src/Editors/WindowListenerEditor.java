48d33bdf2665d46f58c84b4d47f49bbc589bfe1f
Daniel Boydelatour <danielboydelatour@yahoo.com>
2025-02-08 10:25:58 -0500

    add windowlistener editor

---
+++ src/Editors/WindowListenerEditor.java
@@ -0,0 +1,59 @@
+package Editors;
+
+import java.awt.Component;
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
+
+import Params.ParameterEditor;
+
+public class WindowListenerEditor extends ParameterEditor 
+{
+	private static final String 
+		EDITOR_DIRECTORY = "\\src\\WindowListeners\\ ",
+		PACKAGE_PREFIX = "WindowListeners",
+		EDITOR_PARAMETER_FILE_EXTENSION = "\\.java",
+		EDITOR_PARAMETER_FILE_FILTER = "java";
+	
+	private JComboBox<String> comboBox;
+	
+	@Override
+	public Component getComponentEditor() 
+	{
+		if(comboBox == null)
+		{
+			ArrayList<String> obs = loadClassExtensionsAsString(
+					EDITOR_DIRECTORY, 
+					EDITOR_PARAMETER_FILE_EXTENSION, 
+					PACKAGE_PREFIX, 
+					EDITOR_PARAMETER_FILE_FILTER);
+			comboBox = new JComboBox<String>(obs.toArray(new String [obs.size()]));
+		}
+		return comboBox;
+	}
+
+	@Override
+	public void setComponentValue(Object value) 
+	{
+		comboBox.setSelectedItem(value.getClass().getName());//using class name
+	}
+
+	@Override
+	public String[] getComponentValue() 
+	{
+		return new String [] {comboBox.getSelectedItem().toString()};
+	}
+
+	@Override
+	public String getComponentXMLOutput() {
+		return null;
+	}
+
+	@Override
+	public String getParameterDefintionString() 
+	{
+		return WindowListener.class.getName();
+	}
+
+}
