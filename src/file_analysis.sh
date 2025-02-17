fd2b9c886d3d66fdf276fcb30e7f6d445aa0ab5a
Daniel Boydelatour <danielboydelatour@yahoo.com>
2025-02-17 09:39:41 -0500

    update file analysis to allow altering filter and to show total line count

--- src/file_analysis.sh
+++ src/file_analysis.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-find | grep .java | awk '{system("wc -l " $NF)}' | sort -n 
+#print line count of each file.
+
+defaultFilter=.java
+
+if [ $# -eq 0 ]
+then
+	argFile=$defaultFilter
+	echo "apply filter: " $argFile " (pass argument for another file filter)"
+else
+	argFile=$1
+	echo "apply filter: " $argFile
+fi
+
+vals=`find | grep $argFile | awk '{system("wc -l " $NF)}' | sort -n | awk '{system("echo \"" $1 $2 "\"")}'`
+total=0
+for v in ${vals[@]}
+do
+	linecount=`echo $v | egrep -o "^[0-9]*"`
+	total=$((total + linecount))
+	filename=`echo $v | sed 's/^[0-9]*//g'`
+	echo $filename " | Line Count - " $linecount " | Running Total - " $total
+done
+echo "total line count: " $total
