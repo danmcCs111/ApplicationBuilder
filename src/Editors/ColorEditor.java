f3a8736476e5562c125f2735c803874b3978b651
Daniel Boydelatour <danielboydelatour@yahoo.com>
2025-02-16 19:50:49 -0500

    add save/cancel functionality to color editor

--- src/Editors/ColorEditor.java
+++ src/Editors/ColorEditor.java
@@ -1,13 +1,16 @@
 package Editors;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 import Params.ParameterEditor;
 
@@ -16,6 +19,7 @@ public class ColorEditor extends JButton implements ParameterEditor
 	private static final long serialVersionUID = 1992L;
 	
 	private JColorChooser jcc = null;
+	JDialog d;
 	
 	public ColorEditor()
 	{
@@ -24,8 +28,36 @@ public class ColorEditor extends JButton implements ParameterEditor
 		this.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JDialog d = new JDialog();
-				d.add(jcc);
+				if(d == null)
+				{
+					d = new JDialog();
+					d.setLayout(new BorderLayout());
+					d.add(jcc, BorderLayout.CENTER);
+					JButton save = new JButton("Save");
+					save.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							d.dispose();
+							ColorEditor.this.setForeground((Color)jcc.getColor());
+							d = null;
+						}
+					});
+					JButton cancel = new JButton("Cancel");
+					cancel.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							jcc.setColor(ColorEditor.this.getForeground());
+							d.dispose();
+							d = null;
+						}
+					});
+					JPanel southPane = new JPanel(new BorderLayout());
+					JPanel eastPane = new JPanel(new GridLayout(1,2));
+					eastPane.add(save);
+					eastPane.add(cancel);
+					southPane.add(eastPane, BorderLayout.EAST);
+					d.add(southPane, BorderLayout.SOUTH);
+				}
 				d.setVisible(true);
 				d.pack();
 			}
