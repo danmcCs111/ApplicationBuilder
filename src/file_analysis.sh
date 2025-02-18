1a4c15c5c2a90c172b02d4fe15ac70ead61923d9
Daniel Boydelatour <danielboydelatour@yahoo.com>
2025-02-17 19:44:55 -0500

    revert on commit. loaded old version on previous commit

--- src/file_analysis.sh
+++ src/file_analysis.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-
 #print line count of each file.
+
+defaultFilter=.java
+pad="-"
+padLength=60
 
 if [ $# -eq 0 ]
 then
-	argFile=.java
-	echo "apply filter: " $argFile " (pass argument to change)"
+	argFile=$defaultFilter
+	echo "apply filter: " $argFile " (pass argument for another file filter)"
 else
 	argFile=$1
 	echo "apply filter: " $argFile
 fi
 
-find | grep $argFile | awk '{system(" wc -l " $NF)}' | sort -n 
+vals=`find | grep $argFile | awk '{system("wc -l " $NF)}' | sort -n | awk '{system("echo \"" $1 $2 "\"")}'`
 
-vals=`find | grep $argFile | awk '{system("wc -l " $NF)}' | sort -n | awk '{print $1}'`
 total=0
 for v in ${vals[@]}
 do
-	total=$((total + v))
+	padding=""
+	linecount=`echo $v | egrep -o "^[0-9]*"`
+	total=$((total + linecount))
+	filename=`echo $v | sed "s/$linecount//g"`
+	fLen=$(expr length $filename)
+	len=$(( padLength - fLen ))
+	padding=`for i in $(seq 1 $len); do echo -n $pad; done`
+	echo $filename $padding " | Line Count - " $linecount " | Running Total - " $total
 done
 echo "total line count: " $total
