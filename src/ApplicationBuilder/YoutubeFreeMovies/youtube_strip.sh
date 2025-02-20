c8baee8d1376bda9149de8019f6e291df1ff4b93
Daniel Boydelatour <danielboydelatour@yahoo.com>
2025-02-20 10:59:12 -0500

    add pngs download to pull script

--- src/ApplicationBuilder/YoutubeFreeMovies/youtube_strip.sh
+++ src/ApplicationBuilder/YoutubeFreeMovies/youtube_strip.sh
@@ -8,7 +8,6 @@ cat youtube_free.txt | sed 's/"/\n/g' | egrep -o "(ytimg.*)" > img.txt
 cat youtube_free.txt | sed 's/\"/\n/g' | sed 's/:/ /g' | egrep -A6 "title" | grep -A3 text | egrep ^[0-9A-Z]+ > you_name.txt
 
 watchPrefix="watch?v="
-#watchPostfix="\\u0026"
 watchPostfix="\u0026.*"
 backslash="\\\\"
 bottomStripCount=8
@@ -21,7 +20,8 @@ cat you_name.txt | sed -n "$(( topStripCount + 1 )),$(( len - bottomStripCount )
 for i in $(seq 1 $watchLen) 
 do
 	filename=$(cat you_name2.txt | sed -n "$(( i )),$(( i ))p")
-	filenameImg="images/"$filename"_img.url"
+	#filenameImg="images/"$filename"_img.url"
+	filenameImg="images/"$filename".png"
 	filenameVid=$filename".url"
 	vidId="`cat watch.txt | sed -n "$(( i )),$(( i ))p" | sed "s/$watchPrefix//g" | sed "s/$watchPostfix//g" | sed "s/$backslash//g" `"
 
@@ -31,7 +31,8 @@ do
 	echo "[InternetShortcut]" > "$filenameVid"
 	echo "URL=https://youtube.com/"`cat watch.txt | sed -n "$(( i )),$(( i ))p"` >> "$filenameVid"
 
-	echo "[InternetShortcut]" > "$filenameImg"
-	echo "URL=https://i."`cat img.txt | grep "$vidId"` >> "$filenameImg"
+	url=$(echo "https://i."`cat img.txt | grep "$vidId"`)
+	echo $url
+	curl --output "$filenameImg" $url
 done
 rm watch.txt you_name.txt you_name2.txt img.txt
