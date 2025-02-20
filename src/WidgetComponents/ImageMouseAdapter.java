f2b8e3cc2fd3ba6a1ca068e461fe176a5943c293
Daniel Boydelatour <danielboydelatour@yahoo.com>
2025-02-20 11:27:43 -0500

    add start to processing showing images collected. todo add to xml system referenced paths.. from list gather?

--- src/WidgetComponents/ImageMouseAdapter.java
+++ src/WidgetComponents/ImageMouseAdapter.java
@@ -2,25 +2,50 @@ package WidgetComponents;
 
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Label;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import Properties.LoggingMessages;
+import Properties.PathUtility;
 
 public class ImageMouseAdapter extends MouseAdapter
 {
 		private JFrame f;
 		private Component component;
+		private BufferedImage img;
 		private String text;
-		private static final Dimension DIM = new Dimension(350,50);
+		private static final Dimension DIM_PAD = new Dimension(150,0);
+		private static final Dimension DIM = new Dimension(350,450);
 		
-		public ImageMouseAdapter(Component component, String text)
+		public ImageMouseAdapter(Component component, String path, String text)
 		{
 			this.component = component;
 			this.text = text;
+			String fileLocation = path + toPngFilename();
+			File file = new File(fileLocation);
+			LoggingMessages.printOut("file location: " + fileLocation);
+			try {
+				img = ImageIO.read(file);
+				
+			} catch (IOException e) {
+				LoggingMessages.printOut("file not found: " + fileLocation);
+			}
+		}
+		
+		private String toPngFilename()
+		{
+			return this.text + ".png";
 		}
 		
 		@Override
@@ -31,16 +56,22 @@ public class ImageMouseAdapter extends MouseAdapter
 		
 		@Override
 		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-			Point loc = component.getLocation();
 			f = new JFrame();
 			Rectangle bounds = component.getBounds();
 			f.setUndecorated(true);
-			Label l = new Label();
+			JPanel p = new JPanel();
+			JLabel l = new JLabel();
 			l.setText(text);
-			f.setLocation(loc.x+bounds.width, loc.y);
+			p.add(l);
+			if(img != null)
+			{
+				JLabel picLabel = new JLabel(new ImageIcon(img));
+				p.add(picLabel);
+			}
+			
+			f.add(p);
+			f.setLocation(bounds.width + DIM_PAD.width, bounds.height + DIM_PAD.height);
 			f.setMinimumSize(DIM);
-			f.add(l);
 			f.setVisible(true);
 		}
 }
--- src/WidgetComponents/JButtonArray.java
+++ src/WidgetComponents/JButtonArray.java
@@ -3,23 +3,16 @@ package WidgetComponents;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Label;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import Properties.PathUtility;
 import WidgetExtensions.ExtendedStringCollection;
 import WidgetUtility.FileListOptionGenerator;
 
@@ -91,7 +84,10 @@ public class JButtonArray extends JPanel implements ArrayActionListener, Charact
 							((AbstractButton) comp).setText(tmpTxt);
 						}
 //						((AbstractButton) comp).setToolTipText(tmpTxt);
-						((AbstractButton) comp).addMouseListener(new ImageMouseAdapter(comp, tmpTxt));
+						//TODO
+						((AbstractButton) comp).addMouseListener(new ImageMouseAdapter(comp, 
+								PathUtility.getCurrentDirectory() + "/src/ApplicationBuilder/YoutubeFreeMovies/images/", 
+								tmpTxt));
 					}
 					comp.setForeground(backgroundAndForegroundColor[1]);
 					comp.setBackground(backgroundAndForegroundColor[0]);
