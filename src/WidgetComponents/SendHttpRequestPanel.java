650b78fab9d6144c2ac00b52f074ef6ee708cf83
danmcCs111 <danmccs111@gmail.com>
2025-03-12 11:32:03 -0400

    update for sending query to system

--- src/WidgetComponents/SendHttpRequestPanel.java
+++ src/WidgetComponents/SendHttpRequestPanel.java
@@ -6,9 +6,11 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpResponse;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -16,9 +18,14 @@ public class SendHttpRequestPanel extends JPanel
 {
 	private static final long serialVersionUID = 3000L;
 	
-	private static final String SEND_BUTTON_TEXT = "Send Http Get";
+	private static final String 
+		SEND_BUTTON_TEXT = "Send Http Get",
+		REQUEST_TYPE_HEADER_KEY = "Get-request-type";
+	
+	private static final String [] GET_HTTP_OPTIONS = new String [] {"Query"};
 	
 	private JButton sendButton;
+	private JComboBox<String> getType;
 	private JTextArea textArea;
 	
 	private static int
@@ -34,19 +41,24 @@ public class SendHttpRequestPanel extends JPanel
 				execute();
 			}
 		});
+		
+		getType = new JComboBox<String>(GET_HTTP_OPTIONS);
+		
 		textArea = new JTextArea();
-		textArea.setName("UserInput");
 		this.add(sendButton);
+		this.add(getType);
 		this.add(textArea);
 	}
 	
 	private String execute()
 	{
+		BodyPublisher bp = HttpRequest.BodyPublishers.ofString(textArea.getText());
+		
 		HttpRequest request = HttpRequest.newBuilder()
 				.uri(URI.create("http://localhost:" + PORT_NUMBER))
 				.header("Test", "value")
-				.header(textArea.getName(), textArea.getText())
-				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.header(REQUEST_TYPE_HEADER_KEY, getType.getSelectedItem().toString())
+				.method("GET", bp)
 				.build();
 		HttpResponse<String> response = null;
 		try {
