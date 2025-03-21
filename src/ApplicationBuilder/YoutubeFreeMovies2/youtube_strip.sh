e5a90d34dc6bb8c2beba2243d6950854019fa322
danmcCs111 <danmccs111@gmail.com>
2025-03-21 14:49:09 -0400

    add new pull of youtube free movies

---
+++ src/ApplicationBuilder/YoutubeFreeMovies2/youtube_strip.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#todo use array
+rm *.url
+rm images/*.url
+
+cat youtube_free.txt | sed 's/\"/\n/g' | sed 's/:/ /g' | egrep -o "(watch\?.*)" > watch.txt
+cat youtube_free.txt | sed 's/"/\n/g' | egrep -o "(ytimg.*)" > img.txt
+cat youtube_free.txt | sed 's/\"/\n/g' | sed 's/:/ /g' | egrep -A6 "title" | grep -A3 text | egrep ^[0-9A-Z]+ > you_name.txt
+
+watchPrefix="watch?v="
+watchPostfix="\u0026.*"
+backslash="\\\\"
+bottomStripCount=8
+watchLen=`cat watch.txt | wc -l`
+len=`cat you_name.txt | wc -l`
+topStripCount=$(( len - bottomStripCount - watchLen ))
+
+cat you_name.txt | sed -n "$(( topStripCount + 1 )),$(( len - bottomStripCount ))p" > you_name2.txt
+
+for i in $(seq 1 $watchLen) 
+do
+	filename=$(cat you_name2.txt | sed -n "$(( i )),$(( i ))p")
+	#filenameImg="images/"$filename"_img.url"
+	filenameImg="images/"$filename".png"
+	filenameVid=$filename".url"
+	vidId="`cat watch.txt | sed -n "$(( i )),$(( i ))p" | sed "s/$watchPrefix//g" | sed "s/$watchPostfix//g" | sed "s/$backslash//g" `"
+
+	echo "$vidId"
+	echo `cat watch.txt | grep "$vidId" watch.txt`
+
+	echo "[InternetShortcut]" > "$filenameVid"
+	echo "URL=https://youtube.com/"`cat watch.txt | sed -n "$(( i )),$(( i ))p"` >> "$filenameVid"
+
+	url=$(echo "https://i."`cat img.txt | grep "$vidId"`)
+	echo $url
+	curl --output "$filenameImg" $url
+done
+rm watch.txt you_name.txt you_name2.txt img.txt
