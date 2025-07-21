dc26da3f2517eca8a4e8889c72f63a3dc58078de
danmcCs111 <danielboydelatour@gmail.com>
2025-07-20 21:56:53 -0400

    add initial function code for generic shape and generic shape iterators

--- src/ShapeWidgetComponents/GenericShape.java
+++ src/ShapeWidgetComponents/GenericShape.java
@@ -1,25 +1,69 @@
 package ShapeWidgetComponents;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 public class GenericShape implements Shape 
 {
-
-	@Override
-	public Rectangle getBounds() {
-		// TODO Auto-generated method stub
-		return null;
+	Point [] points;
+	Rectangle2D bounds;
+	ArrayList<Integer> pathIterators;
+	
+	public GenericShape(Point [] points, ArrayList<Integer> pathIterators)
+	{
+		this.points = points;
+		this.pathIterators = pathIterators;
+	}
+	
+	public ArrayList<Integer> getPathIterators()
+	{
+		return this.pathIterators;
+	}
+	
+	public Point getPoint(int index)
+	{
+		return this.points[index];
+	}
+	
+	public void constructBounds()
+	{
+		double 
+			leastX = points[0].x,
+			leastY = points[0].y,
+			mostX = points[0].x,
+			mostY = points[0].y;
+		
+		for(int i = 1; i < pathIterators.size(); i++)
+		{
+			Point p = points[i];
+			if(leastX > p.x)
+				leastX = p.x;
+			if(leastY > p.y)
+				leastY = p.y;
+			if(mostX < p.x)
+				mostX =  p.x;
+			if(mostY < p.y)
+				mostY =  p.y;
+		}
+		bounds = new Rectangle2D.Double(leastX, leastY, mostX - leastX, mostY - leastY);
 	}
 
 	@Override
-	public Rectangle2D getBounds2D() {
-		// TODO Auto-generated method stub
-		return null;
+	public Rectangle getBounds() 
+	{
+		return this.bounds.getBounds();
+	}
+
+	@Override
+	public Rectangle2D getBounds2D() 
+	{
+		return this.bounds;
 	}
 
 	@Override
@@ -59,15 +103,15 @@ public class GenericShape implements Shape
 	}
 
 	@Override
-	public PathIterator getPathIterator(AffineTransform at) {
-		// TODO Auto-generated method stub
-		return null;
+	public PathIterator getPathIterator(AffineTransform at) 
+	{
+		return new GenericShapeIterator(this, at);
 	}
 
 	@Override
-	public PathIterator getPathIterator(AffineTransform at, double flatness) {
-		// TODO Auto-generated method stub
-		return null;
+	public PathIterator getPathIterator(AffineTransform at, double flatness) 
+	{
+		return new GenericShapeIterator(this, at);
 	}
 
 }
