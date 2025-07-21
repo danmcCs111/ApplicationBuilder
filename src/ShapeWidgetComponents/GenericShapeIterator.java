b8d20c8804b2d43e0bc193b8e68bea309d3143a0
danmcCs111 <danielboydelatour@gmail.com>
2025-07-20 21:37:08 -0400

    add additional missing class file needed for custom paths

---
+++ src/ShapeWidgetComponents/GenericShapeIterator.java
@@ -0,0 +1,37 @@
+package ShapeWidgetComponents;
+
+import java.awt.geom.PathIterator;
+
+public class GenericShapeIterator implements PathIterator 
+{
+	@Override
+	public int getWindingRule() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int currentSegment(float[] coords) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int currentSegment(double[] coords) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
