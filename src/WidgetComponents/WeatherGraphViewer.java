a37a1363a386f0b99b3b648e6bbed6c61be95d79
danmcCs111 <danmccs111@gmail.com>
2025-08-01 16:42:40 -0400

    add drawing function

--- src/WidgetComponents/WeatherGraphViewer.java
+++ src/WidgetComponents/WeatherGraphViewer.java
@@ -1,5 +1,8 @@
 package WidgetComponents;
 
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.geom.Ellipse2D;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
@@ -7,11 +10,13 @@ import java.util.HashMap;
 import java.util.List;
 
 import ObjectTypeConversion.WeatherReading;
+import ShapeWidgetComponents.ShapeDrawingCollectionGraphics;
 
 public class WeatherGraphViewer extends GraphViewer 
 {
 	private static final long serialVersionUID = 1L;
 	private HashMap<Date, WeatherReading> readings;
+	private HashMap<Date, Point> xYPoints;
 	
 	public WeatherGraphViewer()
 	{
@@ -52,12 +57,37 @@ public class WeatherGraphViewer extends GraphViewer
 			}
 		}
 		//Generate graph.
+		int 
+			height = 600,//this.getHeight();
+			width = 600;//this.getWidth();
+		this.xYPoints = new HashMap<Date, Point>();
 		List<Date> sorted = Arrays.asList(readings.keySet().toArray(new Date[] {}));
 		Collections.sort(sorted);
+		
 		for(Date d : sorted)
 		{
-			findX(600, sorted.get(sorted.size()-1), sorted.get(0), d);
-			findY(600, numberHigh, numberLow, Integer.parseInt(""+readings.get(d).temperature));//Test.
+			Point p = new Point();
+			p.x = findX(width, sorted.get(sorted.size()-1), sorted.get(0), d);
+			p.y = findY(height, numberHigh, numberLow, Integer.parseInt(""+readings.get(d).temperature));//Test.
+			this.xYPoints.put(d, p);
+		}
+		visualize(xYPoints, key);
+	}
+	
+	private void visualize(HashMap<Date, Point> xYPoints, String key)
+	{
+		int plotsize = 6;
+		
+		ShapeDrawingCollectionGraphics.clearAll(this);
+		for(Date d : xYPoints.keySet())
+		{
+			Point p = xYPoints.get(d);
+			Ellipse2D ellipse = new Ellipse2D.Double(
+					p.x- (plotsize / 2), 
+					p.y- (plotsize / 2), 
+					plotsize, 
+					plotsize);
+			ShapeDrawingCollectionGraphics.drawShape(this, ellipse, Color.blue);
 		}
 	}
 
