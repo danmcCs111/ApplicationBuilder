325bfdcd69001b9cedb587c05e7af90a6d08c893
danmcCs111 <danmccs111@gmail.com>
2025-08-09 20:20:13 -0400

    add weather updater as scheduled command

--- src/Actions/WeatherDatabaseUpdater.java
+++ src/Actions/WeatherDatabaseUpdater.java
@@ -16,25 +16,24 @@ public class WeatherDatabaseUpdater
 {
 	public static final String
 		GIT_BASH = "C:\\Program Files\\Git\\git-bash.exe",
-		CSV_FILE_LOCATION = "C:\\Users\\danie\\codebase\\danmcCs111\\PlayrightCopyUrl\\PlayrightVideo\\GrabFolder\\Weather\\weather_grab";
+		CSV_FILE_PREFIX = "weather_grab";
 	public static final SimpleDateFormat SDF_FILE = new SimpleDateFormat("_YYYY_M_d_h_m");
 	public static final String []//TODO
-		OPTIONS = new String [] {"-c"},
-		UPLOAD_SCRIPT_PATH = new String [] {"/c/Users/danie/codebase/danmcCs111/PlayrightCopyUrl/PlayrightVideo/collectWeatherToCsv.sh"};
+		OPTIONS = new String [] {"-c"};
 	
 	private CommandBuild cb;
 	
-	public WeatherDatabaseUpdater()
+	public WeatherDatabaseUpdater(String weatherFileLocation, String uploadScriptPath)
 	{
 		Date d = Calendar.getInstance().getTime();
 		String suffix = SDF_FILE.format(d);
 		LoggingMessages.printOut(suffix);//TODO. filename as param.
 		cb = new CommandBuild(GIT_BASH);
-		cb.setCommand(GIT_BASH, OPTIONS, UPLOAD_SCRIPT_PATH);
+		cb.setCommand(GIT_BASH, OPTIONS, new String [] {uploadScriptPath});
 		try {
 			CommandExecutor.executeProcess(cb, true);
 			
-			CsvReader cr = new CsvReader(CSV_FILE_LOCATION + suffix + ".csv");
+			CsvReader cr = new CsvReader(weatherFileLocation + CSV_FILE_PREFIX + suffix + ".csv");
 			cr.read();
 			WeatherGrabCsvConverter wgcc = new WeatherGrabCsvConverter(cr);
 			HashMap<Date, WeatherReading> weatherReadings = wgcc.getWeatherReadings();
@@ -54,7 +53,11 @@ public class WeatherDatabaseUpdater
 	
 	public static void main(String [] args)
 	{
-		WeatherDatabaseUpdater wdu = new WeatherDatabaseUpdater();
+		String 
+			weatherFileLocation = args[0],
+			uploadScriptPath = args[1];
+		
+		WeatherDatabaseUpdater wdu = new WeatherDatabaseUpdater(weatherFileLocation, uploadScriptPath);
 		LoggingMessages.printOut(wdu.getCommandBuild().getArgs());
 	}
 }
