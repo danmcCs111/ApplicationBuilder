546e285d56c5e95b42e53104c362877b7f2ee7d9
danmcCs111 <danmccs111@gmail.com>
2025-11-05 22:08:48 -0500

    adjust loading process test. need to switch to images for performance

--- src/ShapeWidgetComponents/LoadingRunnable.java
+++ src/ShapeWidgetComponents/LoadingRunnable.java
@@ -8,54 +8,53 @@ import java.util.ArrayList;
 public class LoadingRunnable implements Runnable
 {
 	public static double 
-		DEGREE_SHIFT = 50.0;
-	public static int SLEEP_MILLIS = 200;
+		DEGREE_SHIFT_TIMES = 20.0;
+	public static int SLEEP_MILLIS = 150;
 	
 	private Container drawContainer;
-	private ShapeDrawingCollection sdc;
 	private Shape originalShape;
 	private ArrayList<Point> cps;
 	private ShapeStyling ss;
+	private ArrayList<Shape> shapes = new ArrayList<Shape>();
 	
 	public LoadingRunnable(Container drawContainer, ShapeDrawingCollection sdc)
 	{
 		this.drawContainer = drawContainer;
-		this.sdc = sdc;
 		
-		originalShape = sdc.getShapes().get(0);//TODO;
+		originalShape = sdc.getShapes().get(0);//TODO; switch to images...
 		cps = sdc.getShapeControlPoints().get(0);
 		ss = sdc.getShapeStylings().get(0);
+	}
+	
+	private void loadShapes()
+	{
+		double divCount = 360.0 / DEGREE_SHIFT_TIMES;
+		
+		for(int i = 1; i < DEGREE_SHIFT_TIMES; i++)
+		{
+			ArrayList<Point> recalcPoints = ShapeUtils.rotate(originalShape, cps, divCount * i);
+			Shape newShape = ShapeUtils.recalculateShape(originalShape, recalcPoints);
+			shapes.add(newShape);
+		}
 	}
 	
 	@Override
 	public void run() 
 	{
-		int count = 1;
-		try {
-			Thread.sleep(SLEEP_MILLIS);//TODO
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		loadShapes();
 		do
 		{
-			try {
-				Thread.sleep(SLEEP_MILLIS);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			for(Shape shape : shapes)
+			{
+				ShapeDrawingCollectionGraphics.clearAll(drawContainer);
+				ShapeDrawingCollectionGraphics.drawShape(drawContainer, shape, ss);
+				try {
+					Thread.sleep(SLEEP_MILLIS);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
-			double degrees = (DEGREE_SHIFT * count);
-			rotate(degrees, sdc);
-			count++;
 		} while(true);
 	}
 	
-	private void rotate(double degrees, ShapeDrawingCollection sdc)
-	{
-		ArrayList<Point> recalcPoints = ShapeUtils.rotate(originalShape, cps, degrees);
-		Shape newShape = ShapeUtils.recalculateShape(originalShape, recalcPoints);
-		
-		ShapeDrawingCollectionGraphics.clearAll(drawContainer);
-		ShapeDrawingCollectionGraphics.drawShape(drawContainer, newShape, ss);
-	}
-
 }
