ccc7ed1c7640ca4c4bab9db13938c45fdcd7d01d
danmcCs111 <danmccs111@gmail.com>
2025-11-29 18:51:43 -0500

    break fix. was using old directory

--- VideoLaunchFiles/Tubi/importTubi.sh
+++ VideoLaunchFiles/Tubi/importTubi.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 scriptDirectory="$1"
 tubiDirectory=`pwd`
 
-scriptName="/GrabScripts/collectAllTubiUrls.sh"
+scriptName="/GrabScripts/Tubi/collectAllTubiUrls.sh"
 tubiUrls="/GrabFolder/Tubi/*.url"
 
 script=$scriptDirectory$scriptName
