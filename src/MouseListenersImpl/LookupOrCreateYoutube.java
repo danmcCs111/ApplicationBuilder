374efc91da0c0e7e9c3d85bf7a58201b17912d90
danmcCs111 <danmccs111@gmail.com>
2025-12-30 20:51:08 -0500

    create youtube video lookup methods

--- src/MouseListenersImpl/LookupOrCreateYoutube.java
+++ src/MouseListenersImpl/LookupOrCreateYoutube.java
@@ -1,23 +1,32 @@
 package MouseListenersImpl;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import HttpDatabaseRequest.HttpDatabaseRequest;
 import HttpDatabaseRequest.SelectWebServiceQueries;
 import HttpDatabaseResponse.DatabaseResponseNode;
 import HttpDatabaseResponse.HttpDatabaseResponse;
+import Properties.LoggingMessages;
 import Properties.PathUtility;
 
 public class LookupOrCreateYoutube 
 {
 	public static final String 
+		YOUTUBE_CHANNEL_HANDLE_STRIP = "/[^/]*$",
+		OPERATION = "showResult",
+		SAVE_INSERT_PATH = "./VideoLaunchFiles/YoutubeChannels/video-images/",
+		KEY_PATH = "C:\\Users\\danie\\OneDrive\\Documents\\api-key.txt", //TODO
 		YOUTUBE_QUERY_PREFIX = "SELECT * FROM videodatabase.video WHERE VideoName_Video_VideoDatabase = ",
 		YOUTUBE_QUERY_SUFFIX = ";",
-		YOUTUBE_INSERT_PREFIX = "INSERT INTO videodatabase.video (VideoName_Video_VideoDatabase, VideoUrl_Video_VideoDatabase) values( ",
-		YOUTUBE_INSERT_SUFFIX = ");",
-		IS_LOOKUP_FRAME_FILTER = "";
-	
-	public static final String 
+		YOUTUBE_VIDEO_QUERY_PREFIX = "SELECT * FROM videodatabase.videoYoutube WHERE ParentID_VideoYoutube_VideoYoutubeDatabase = ",
+		YOUTUBE_VIDEO_QUERY_SUFFIX = " ORDER BY UploadDate_VideoYoutube_VideoYoutubeDatabase DESC;",
+		YOUTUBE_INSERT_PREFIX = "INSERT INTO videodatabase.video (VideoName_Video_VideoDatabase, VideoUrl_Video_VideoDatabase, InsertDate_Video_VideoDatabase) values( ",
+		YOUTUBE_INSERT_SUFFIX = " CURRENT_TIMESTAMP);",
+		IS_LOOKUP_FRAME_FILTER = "",
 		ENDPOINT = "http://localhost:",
 		REQUEST_TYPE_HEADER_KEY = "Get-request-type",
 		REQUEST_TYPE_HEADER_VALUE_QUERY = "Query",
@@ -32,7 +41,7 @@ public class LookupOrCreateYoutube
 		
 	}
 	
-	public void lookup(String videoChannelName, String url)
+	public void lookup(String videoChannelName, String videoChannelLink)
 	{
 		String query = YOUTUBE_QUERY_PREFIX + PathUtility.surroundString(videoChannelName, "\"") + YOUTUBE_QUERY_SUFFIX;
 		
@@ -48,19 +57,30 @@ public class LookupOrCreateYoutube
 		ArrayList <ArrayList <DatabaseResponseNode>> drns = hdr.parseResponse(response);
 		if(drns.isEmpty())
 		{
-			createIfEmpty(videoChannelName, url);
+			createIfEmpty(videoChannelName, videoChannelLink);
 		}
-		
+		else
+		{
+			for(DatabaseResponseNode drn : drns.get(1))
+			{
+				if(drn.getNodeName().equals("VideoId_Video_VideoDatabase"))
+				{
+					int parentId = Integer.parseInt(drn.getNodeAttributes().get("content"));
+					LoggingMessages.printOut("parentID is: " + parentId);
+					lookupYoutubeVideo(parentId, videoChannelLink);
+				}
+			}
+		}
 	}
 	
 	private void createIfEmpty(String videoChannelName, String url)
 	{
 		String insert = YOUTUBE_INSERT_PREFIX + 
 				PathUtility.surroundString(videoChannelName, "\"") + ", " + 
-				PathUtility.surroundString(url, "\"") +
+				PathUtility.surroundString(url, "\"") + ", " +
 				YOUTUBE_INSERT_SUFFIX;
 		
-		String response = HttpDatabaseRequest.executeGetRequest
+		HttpDatabaseRequest.executeGetRequest
 		(
 				SelectWebServiceQueries.ENDPOINT,
 				SelectWebServiceQueries.PORT_NUMBER,
@@ -68,8 +88,75 @@ public class LookupOrCreateYoutube
 				SelectWebServiceQueries.REQUEST_TYPE_HEADER_KEY,
 				REQUEST_TYPE_HEADER_VALUE_INSERT
 		);
+		lookup(videoChannelName, url);
+		
 //		HttpDatabaseResponse hdr = new HttpDatabaseResponse();
 //		ArrayList <ArrayList <DatabaseResponseNode>> drns = hdr.parseResponse(response);
+	}
+	
+	public void lookupYoutubeVideo(int parentId, String videoChannelLink)
+	{
+		String query = YOUTUBE_VIDEO_QUERY_PREFIX + parentId + YOUTUBE_VIDEO_QUERY_SUFFIX;
+		
+		String response = HttpDatabaseRequest.executeGetRequest
+		(
+				SelectWebServiceQueries.ENDPOINT,
+				SelectWebServiceQueries.PORT_NUMBER,
+				query,
+				SelectWebServiceQueries.REQUEST_TYPE_HEADER_KEY,
+				REQUEST_TYPE_HEADER_VALUE_QUERY
+		);
+		HttpDatabaseResponse hdr = new HttpDatabaseResponse();
+		ArrayList <ArrayList <DatabaseResponseNode>> drns = hdr.parseResponse(response);
+		Date lastDate = null;
+		if(drns.isEmpty())
+		{
+			LoggingMessages.printOut("Empty");
+		}
+		else
+		{
+			for(DatabaseResponseNode drn : drns.get(1))
+			{
+				if(drn.getNodeName().equals("UploadDate_VideoYoutube_VideoYoutubeDatabase"))
+				{
+					long timestamp = Long.valueOf(drn.getNodeAttributes().get("content"));
+					lastDate = new Date(timestamp);
+					LoggingMessages.printOut("last time: " + lastDate);
+					break;
+				}
+			}
+		}
+		getYoutubeVideos(parentId, videoChannelLink, (lastDate == null) ? -1 : lastDate.getTime());
+	}
+	
+	private void getYoutubeVideos(int parentId, String videoChannelLink, long lastDate)
+	{
+		Pattern pattern = Pattern.compile(YOUTUBE_CHANNEL_HANDLE_STRIP);
+		Matcher m = pattern.matcher(videoChannelLink);
+		String youtubeHandle = "";
+		if(m.find())
+		{
+			youtubeHandle = m.group();
+		}
+		youtubeHandle = youtubeHandle.replace("/", "");
+		
+		LoggingMessages.printOut("Parent ID: " + parentId);
+		LoggingMessages.printOut("Channel Link: " + videoChannelLink);
+		LoggingMessages.printOut("Handle: " + youtubeHandle);
+		LoggingMessages.printOut("Epoch Time: " + lastDate);
+		
+		String youtubeHandleMinusAt = youtubeHandle.replace("@", "");
+		String key = PathUtility.readFileToString(new File(KEY_PATH));
+		String [] args = new String [] {
+			OPERATION, 
+			key,
+			parentId + "", 
+			youtubeHandle, 
+			lastDate + "", 
+			SAVE_INSERT_PATH + youtubeHandleMinusAt
+		};
+		//run plugin.
+		
 	}
 	
 	public static void main(String [] args)
