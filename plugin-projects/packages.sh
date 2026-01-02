a1c77d32aee54acca9d75aece5a58aecf4e6d304
danmcCs111 <danmccs111@gmail.com>
2026-01-02 14:55:12 -0500

    update packages list

--- plugin-projects/packages.sh
+++ plugin-projects/packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-projects=("https://github.com/danmcCs111/SeleniumPython.git" "https://github.com/danmcCs111/PlayrightCopyUrl.git" "https://github.com/danmcCs111/AutoHotKey-Utils.git" "https://github.com/danmcCs111/YouTube-API-list.git")
+projects=("https://github.com/danmcCs111/SeleniumPython.git" "https://github.com/danmcCs111/PlayrightCopyUrl.git" "https://github.com/danmcCs111/AutoHotKey-Utils.git" "https://github.com/danmcCs111/YouTube-API-list.git" "https://github.com/danmcCs111/DatabaseAdapterService.git")
