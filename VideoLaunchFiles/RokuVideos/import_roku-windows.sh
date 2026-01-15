6e8cf8d12c91911678c487d8f6283d2afa6c5379
danmcCs111 <danmccs111@gmail.com>
2026-01-15 11:15:01 -0500

    add copy to script

--- VideoLaunchFiles/RokuVideos/import_roku-windows.sh
+++ VideoLaunchFiles/RokuVideos/import_roku-windows.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #path="$1"
 path=`pwd`"/plugin-projects/SeleniumPython/"
+
+
 cd "$(dirname "$0")"
-rm *.url
-cd $path
-rm GrabFolder/Roku/*.url
-start "rokuCategoryGrab.cmd"
+cp "$path"GrabFolder/Roku/*.url .
+cp "$path"GrabFolder/Roku/images/*.png images/
