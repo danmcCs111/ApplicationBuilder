ccb6d9330711e72b78e6ee72eec4c5ea106a6163
danmcCs111 <danmccs111@gmail.com>
2026-01-15 06:00:57 -0500

    adjust roku script

--- VideoLaunchFiles/RokuVideos/import_roku-windows.sh
+++ VideoLaunchFiles/RokuVideos/import_roku-windows.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #path="$1"
-path="plugin-projects/SeleniumPython/"
+path=`pwd`"/plugin-projects/SeleniumPython/"
+cd "$(dirname "$0")"
+rm *.url
 cd $path
+rm GrabFolder/Roku/*.url
 start "rokuCategoryGrab.cmd"
