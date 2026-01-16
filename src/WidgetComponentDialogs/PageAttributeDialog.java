5845ec43f6e201f8dd382a048fb08950b38a4bc8
danmcCs111 <danmccs111@gmail.com>
2026-01-16 18:25:23 -0500

    remove unused

--- src/WidgetComponentDialogs/PageAttributeDialog.java
+++ src/WidgetComponentDialogs/PageAttributeDialog.java
@@ -1,6 +1,5 @@
 package WidgetComponentDialogs;
 
-import java.awt.Dimension;
 import java.util.ArrayList;
 
 import WidgetComponentInterfaces.EditButtonArrayUrls;
@@ -12,13 +11,7 @@ public class PageAttributeDialog extends CollectionEditor
 	
 	private static String
 		TITLE_TEXT = "Edit Page Attribute",
-		ADD_ATTRIBUTE_TEXT = "Enter New Attribute: ",
-		ADD_PARSER_BUTTON_TEXT = "+ Add Page Parser",
-		DELETE_PARSER_BUTTON_TEXT = "X",
-		SAVE_BUTTON_LABEL = "Save",
-		CANCEL_BUTTON_LABEL = "Cancel";
-	private static final Dimension 
-		MIN_DIMENSION_DIALOG = new Dimension(400, 600);
+		ADD_ATTRIBUTE_TEXT = "Enter New Attribute: ";
 	
 	private static PageAttributeDialog pad = null;
 	private static EditButtonArrayUrls ebau;
