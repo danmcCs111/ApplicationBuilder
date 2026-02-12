a32f22b1574adf40e59991fb8a50a9a0d57d9eaf
danmcCs111 <danmccs111@gmail.com>
2026-02-12 15:15:17 -0500

    update gogUpload using new utility class, add tag/column mapping

--- src/ApplicationBuilder/GogUpload.java
+++ src/ApplicationBuilder/GogUpload.java
@@ -2,18 +2,30 @@ package ApplicationBuilder;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.HashMap;
 
 import Properties.LoggingMessages;
 import Properties.PathUtility;
+import Properties.StringUtility;
 
 public class GogUpload 
 {
 	private static final String 
+		gameMatch = "\\([^\\)]*\\)",
+		valueMatch = "[^\\|\\(]*\\|[^\\|]*\\|",
+		tagMatch = ",\\|[^\\|]*\\|",
 		gogFiles = PathUtility.getCurrentDirectory() + "/plugin-projects/SeleniumPython/gog/tmp/";
+	private static char [] 
+		stripCharsTag = new char[] {',', '|'};
 	private static final String []
 			gogFileFilter = new String [] {"formatted-page", ".txt.tmp"};
+	private static final HashMap<String, String> textTagAndColumnName = new HashMap<String, String>();
+	static {
+		textTagAndColumnName.put("link", "GameUrl_Game_GameDatabase");
+		textTagAndColumnName.put("title", "GameTitle_Game_GameDatabase");
+		textTagAndColumnName.put("final-value", "GameFinalValue_GameCost_GameDatabase");
+		textTagAndColumnName.put("base-value", "GameBaseValue_GameCost_GameDatabase");
+	}
 	
 	public static void main(String [] args)
 	{
@@ -24,13 +36,21 @@ public class GogUpload
 			String fileContents = PathUtility.readFileToString(new File(gogFiles + s));
 			fileContents = fileContents.substring(1, fileContents.length()-1) + ")"; //specific to page formatting ehh...
 			
-			String gameMatch = "\\([^\\)]*\\)";
-			Pattern pattern = Pattern.compile(gameMatch);
-			Matcher matcher = pattern.matcher(fileContents);
-			while (matcher.find()) 
+			ArrayList<String> games = StringUtility.getMatches(fileContents, gameMatch);
+			for(String game : games)
 			{
-	            LoggingMessages.printOut(matcher.group());
-	        }
+				ArrayList<String> gameValues = StringUtility.getMatches(game, valueMatch);
+				for(String gameValue : gameValues)
+				{
+					ArrayList<String> tags = StringUtility.getMatches(gameValue, tagMatch);
+					for(String tag : tags)
+					{
+						tag = StringUtility.stripChars(tag, stripCharsTag).strip();
+						LoggingMessages.printOut(tag + " " + textTagAndColumnName.get(tag));
+					}
+				}
+			}
 		}
 	}
+	
 }
---
+++ src/Properties/StringUtility.java
@@ -0,0 +1,32 @@
+package Properties;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public interface StringUtility 
+{
+	public static ArrayList<String> getMatches(String textToMatch, String regexMatch)
+	{
+		ArrayList<String> values = new ArrayList<String>();
+		
+		Pattern pattern = Pattern.compile(regexMatch);
+		Matcher matcher = pattern.matcher(textToMatch);
+		while (matcher.find()) 
+		{
+			String value = matcher.group();
+			values.add(value);
+		}
+		return values;
+	}
+	
+	public static String stripChars(String text, char ... charactersToStrip)
+	{
+		for(char c : charactersToStrip)
+		{
+			String s = c + "";
+			text = text.replace(s, "");
+		}
+		return text;
+	}
+}
