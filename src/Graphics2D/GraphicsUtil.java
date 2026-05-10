d8864486f458b598bdc9499841f80e2b9a0a4fd8
danmcCs111 <danmccs111@gmail.com>
2026-05-10 14:45:09 -0400

    update to limit height to at least top of screen

--- src/Graphics2D/GraphicsUtil.java
+++ src/Graphics2D/GraphicsUtil.java
@@ -31,8 +31,12 @@ public interface GraphicsUtil
 			rh = referenceComponent.getHeight(),
 			w = comp.getWidth(),
 			h = comp.getHeight();
+		int 
+			x = loc.x + (int)((rw/2.0) - (w / 2.0)),
+			y = loc.y + (int)((rh/2.0) - (h/2.0));
+		y=(y<0)?0:y;
 		
-		comp.setLocation(loc.x + (int)((rw/2.0) - (w / 2.0)), loc.y + (int)((rh/2.0) - (h/2.0)) );
+		comp.setLocation(x, y);
 	}
 	public static void centerHeightOnlyWindow(Container referenceComponent, Container comp)
 	{
@@ -42,8 +46,12 @@ public interface GraphicsUtil
 			rw = referenceComponent.getWidth(),
 			rh = referenceComponent.getHeight(),
 			h = comp.getHeight();
+		int 
+			x = (int)(loc.x + (rw / 2.0)),
+			y = (int)(loc.y + (rh/2.0 - h/2.0));
+		y=(y<0)?0:y;
 		
-		comp.setLocation((int)(loc.x + (rw / 2.0)), (int)(loc.y + (rh/2.0 - h/2.0)));
+		comp.setLocation(x, y);
 	}
 	public static void centerReferenceOnlyWindow(Container referenceComponent, Container comp)
 	{
@@ -52,8 +60,12 @@ public interface GraphicsUtil
 		double 
 			rw = referenceComponent.getWidth(),
 			rh = referenceComponent.getHeight();
+		int
+			x = (int)(loc.x + (rw / 2.0)),
+			y = (int)(loc.y + (rh/2.0));
+		y=(y<0)?0:y;
 		
-		comp.setLocation((int)(loc.x + (rw / 2.0)), (int)(loc.y + (rh/2.0)));
+		comp.setLocation(x, y);
 	}
 	public static void centerOnScreen(Component comp)
 	{
@@ -65,8 +77,12 @@ public interface GraphicsUtil
 			rh = screenSize.getHeight(),
 			w = comp.getWidth(),
 			h = comp.getHeight();
+		int
+			x = loc.x + (int)((rw/2.0) - (w / 2.0)),
+			y = loc.y + (int)((rh/2.0) - (h/2.0));
+		y=(y<0)?0:y;
 		
-		comp.setLocation(loc.x + (int)((rw/2.0) - (w / 2.0)), loc.y + (int)((rh/2.0) - (h/2.0)) );
+		comp.setLocation(x, y);
 	}
 	
 	public static void rightEdgeTopWindow(Container referenceComponent, Container comp)
@@ -77,12 +93,14 @@ public interface GraphicsUtil
 	public static void rightEdgeTopWindow(Container referenceComponent, Container comp, int numberOfMatchedVisible)
 	{
 		Point loc = referenceComponent.getLocation();
-		int rw = referenceComponent.getWidth();
-		int h = comp.getHeight();
+		int 
+			rw = referenceComponent.getWidth(),
+			h = comp.getHeight(),
+			x = loc.x + (rw), 
+			y = loc.y + (h * (numberOfMatchedVisible));
 		
 		Point locPoint = new Point( 
-			loc.x + (rw), 
-			loc.y + (h * (numberOfMatchedVisible))
+				x,(y<0)?0:y
 		);
 		
 		comp.setLocation(locPoint);
@@ -95,9 +113,12 @@ public interface GraphicsUtil
 		int 
 			rw = referenceComponent.getWidth(),
 			rh = referenceComponent.getHeight(),
-			h = comp.getHeight();
+			h = comp.getHeight(),
+			x = loc.x + rw,
+			y = loc.y + (rh/2 - h/2);
+		y=(y<0)?0:y;
 		
-		comp.setLocation(loc.x + rw, loc.y + (rh/2 - h/2));
+		comp.setLocation(x, y);
 	}
 	
 	public static Image getImageFromXml(int width, int height, File defaultImageLocation, Color backgroundColor)
