b0e46fbd5bff0f3a2afddde8da9d63a83caef3bd
danmcCs111 <danmccs111@gmail.com>
2026-07-05 15:50:08 -0400

    add county page finder

--- src/WidgetComponents/GeoNamesUploader.java
+++ src/WidgetComponents/GeoNamesUploader.java
@@ -1,5 +1,9 @@
 package WidgetComponents;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import ObjectTypeConversion.PageParser;
@@ -9,16 +13,34 @@ public class GeoNamesUploader extends JPanel
 	private static final long serialVersionUID = 1L;
 	
 	private static PageParser 
-		pageParser = null;
+		pageParserCounty = null,
+		pageParserCollectData = null;
 
+	private JButton
+		collectButton;
 	
 	public GeoNamesUploader()
 	{
 		
 	}
 	
+	private void buildWidgets()
+	{
+		collectButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+	}
+	
+	public static void setPageParserDataCollect(PageParser pp)
+	{
+		pageParserCollectData = pp;
+	}
+	
 	public static void setPageParser(PageParser pp)
 	{
-		pageParser = pp;
+		pageParserCounty = pp;
 	}
 }
