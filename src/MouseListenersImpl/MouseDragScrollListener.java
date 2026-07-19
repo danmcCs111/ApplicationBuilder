55743f2a3f0eff29cd1a91474a3869838bc25208
danmcCs111 <danmccs111@gmail.com>
2026-07-19 10:05:45 -0400

    fix mouse drag to respond to reverse direction setting.

--- src/MouseListenersImpl/MouseDragScrollListener.java
+++ src/MouseListenersImpl/MouseDragScrollListener.java
@@ -1,6 +1,7 @@
 package MouseListenersImpl;
 
 import java.awt.AWTException;
+import java.awt.Component;
 import java.awt.Point;
 import java.awt.Robot;
 import java.awt.event.MouseAdapter;
@@ -9,6 +10,7 @@ import java.awt.event.MouseEvent;
 public class MouseDragScrollListener extends MouseAdapter
 {
 	 private Point 
+	 	compLastLocation,
 	 	startPoint;
 	 
 	 private static int
@@ -24,28 +26,47 @@ public class MouseDragScrollListener extends MouseAdapter
 	 {
 		 MOUSE_WHEEL_SPIN = spin;
 	 }
-
+	 
      @Override
      public void mousePressed(MouseEvent e) 
      {
          startPoint = e.getPoint();
      }
-
+     
      @Override
      public void mouseDragged(MouseEvent e) 
      {
+    	 Component comp = (Component) e.getSource();
+    	 Point p = comp.getLocationOnScreen();
+    	 
+    	 int movedDiff = 0;
+    	 if(compLastLocation != null)
+    	 {
+    		 movedDiff = compLastLocation.y - p.y;
+    	 }
+    	 compLastLocation = p;
+    	 
          Point currentPoint = e.getPoint();
-         int dy = startPoint.y - currentPoint.y;
-         startPoint = currentPoint;
+       	 int dy = (startPoint.y + movedDiff) - currentPoint.y;
+         int spin = MOUSE_WHEEL_SPIN;
          
          Robot robot;
 		try {
 			robot = new Robot();
-			robot.mouseWheel(dy > 0 ? -MOUSE_WHEEL_SPIN : MOUSE_WHEEL_SPIN);
+			if(dy > 0)
+			{
+				robot.mouseWheel(-spin);
+			}
+			else if(dy < 0)
+			{
+				robot.mouseWheel(spin);
+			}
+			startPoint = currentPoint;
 			robot.delay(MOUSE_DRAG_DELAY);
 		} catch (AWTException e1) {
 			e1.printStackTrace();
 		}
 
      }
+     
 }
